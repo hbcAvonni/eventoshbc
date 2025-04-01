fa2192bd0ad28e26e3b810f3c80383e1ea5f5d5a
ing-enuarlara <enuar2110@gmail.com>
2025-04-01 19:01:39 +0200

    Archivos para datos criticos

---
+++ sensitive.php
@@ -0,0 +1,26 @@
+<?php
+define('ENVIROMENT', 'PROD');
+define('ID_EMPRESA', 2);
+
+define('EMAIL_SERVER', 'smtp.ionos.es.');
+define('EMAIL_USER', 'contacto@crudosxc.com');
+define('NAME_SENDER', 'CRUDOSXC');
+define('EMAIL_PASSWORD', 'Robotica777@');
+define('EMAIL_PORT', '587');
+
+define('ESTADO_EMAIL_ENVIADO', 'enviado');
+define('ESTADO_EMAIL_ERROR', 'error');
+
+define('CRUDOSXC_LOGO', 'https://crudosxc.com/images/logocrudo.jpg');
+define('CRUDOSXC_COLOR_UNO', '#6017dc');
+define('CRUDOSXC_COLOR_DOS', '#41c4c4');
+define('CRUDOSXC_COLOR_TRES', '#56ffe4');
+
+define('RUTA_ADMIN', 'https://adminocb.oceanblue.es/');
+
+define('TOKEN_X', 'AAAAAAAAAAAAAAAAAAAAAO2vygEAAAAAlRpm5FnQAdC%2FpyYtM%2FiiKToobJM%3DajJ8tNMEA9xeJBLZTmfJQzM7oscQps1Ka5E1D2Oykxlk1DUQZH');
+
+define('ID_META', '1016565783617233');
+define('TOKEN_META', 'd16685af502c98574b3faa4d2f2d87f1');
+
+define('STRIPE_SECRETS_KEY', 'sk_test_51QoYy41ijOl6KJDEUSmtO9AH7M0mfO1YrU8BjHZolHRrBQRUKTkQ0aebvfTV3ADPZChn9WHN1AZVcyoLprrwt00t008Kypx6P6');
