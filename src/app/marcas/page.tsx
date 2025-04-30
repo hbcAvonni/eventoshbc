b38ae8dd32f48cfd01155317c1ad4b53556825d9
ing-enuarlara <enuar2110@gmail.com>
2025-04-30 18:10:34 +0200

    Se traen las marcas dinamicamente

--- src/app/marcas/page.tsx
+++ src/app/marcas/page.tsx
@@ -1,21 +1,40 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Image from "next/image";
 
-const brands = [
-  {
-    id: 1,
-    name: "Marca 1",
-    image: "./assets/images/fibro.png",
-  },
-  {
-    id: 2,
-    name: "Marca 2",
-    image: "./assets/images/albatros.png",
-  },
-];
+interface Brand {
+  scbl_id: number;
+  scbl_nombre: string;
+  scbl_web: string;
+  scbl_direccion: string;
+  scbl_imagen: string;
+}
 
 export default function MarcasPage() {
+  const [brands, setBrands] = useState<Brand[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchBrands = async () => {
+      try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/getBrands`);
+        if (!res.ok) throw new Error("Error al obtener las marcas");
+        const data = await res.json();
+        setBrands(data.rows);
+      } catch (error: unknown) {
+        setError(error instanceof Error ? error.message : "Error desconocido");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchBrands();
+  }, []);
+
   return (
     <>
       <Navbar />
@@ -39,20 +58,33 @@ export default function MarcasPage() {
         <div className="bg-white py-16">
           <div className="container mx-auto px-4">
             <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 items-center">
-              {brands.map((brand) => (
-                <div
-                  key={brand.id}
-                  className="flex items-center justify-center p-6 bg-gray-100 rounded-xl shadow-md hover:shadow-xl transition duration-300"
-                >
-                  <Image
-                    src={brand.image}
-                    alt={brand.name}
-                    width={200}
-                    height={100}
-                    className="object-contain"
-                  />
-                </div>
-              ))}
+              {
+                loading ? (
+                  <p className="text-center text-gray-500">Cargando marcas...</p>
+                ) : error ? (
+                  <p className="text-center text-red-500">{error}</p>
+                ) : ( brands.map((brand) => (
+                  <a
+                    href={brand.scbl_web}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    key={brand.scbl_id}
+                  >
+                    <div
+                      key={brand.scbl_id}
+                      className="flex items-center justify-center p-6 bg-gray-100 rounded-xl shadow-md hover:shadow-xl transition duration-300"
+                    >
+                      <Image
+                        src={brand.scbl_imagen}
+                        alt={brand.scbl_nombre}
+                        width={200}
+                        height={100}
+                        className="object-contain"
+                      />
+                    </div>
+                  </a>
+                )))
+              }
             </div>
           </div>
         </div>
