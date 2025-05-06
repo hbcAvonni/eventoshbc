7d931125abe410c3749c9ca00e7f0d4ae3177bbd
ing-enuarlara <enuar2110@gmail.com>
2025-05-06 20:38:19 +0200

    Se usas usuarios de la BD

--- pages/api/generate-jwt.js
+++ pages/api/generate-jwt.js
@@ -1,20 +1,28 @@
 import jwt from 'jsonwebtoken';
-import { withCors } from '../../lib/withCors'; // Ajusta el path según tu estructura
+import mysql from 'mysql2/promise';
+import { withCors } from '../../lib/withCors';
 
 const SECRET_KEY = process.env.SECRET_KEY;
-const ADMIN_USERNAME = process.env.ADMIN_USERNAME;
-const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
 
 export default withCors(async function handler(req, res) {
   if (req.method === 'POST') {
     const { username, password } = req.body;
 
-    if (username === ADMIN_USERNAME && password === ADMIN_PASSWORD) {
-      const token = jwt.sign(
-        { user: 'admin' },
-        SECRET_KEY,
-        { expiresIn: '1h' }
-      );
+    const db = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    });
+
+    const [rows] = await db.execute(
+      `SELECT * FROM usuarios WHERE usr_clave = SHA1(?) AND (usr_login = ? OR usr_email = ?) AND (usr_tipo = 1 OR usr_tipo = 2)`,
+      [password, username, username]
+    );
+
+    if (rows.length > 0) {
+      const token = jwt.sign({ user: rows[0].usr_login }, SECRET_KEY, { expiresIn: '1h' });
 
       return res.status(200).json({ token });
     } else {
