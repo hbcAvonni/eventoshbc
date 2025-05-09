49d82ef2d2e9740606faa53d61d584ab9d57492d
ing-enuarlara <enuar2110@gmail.com>
2025-05-09 15:46:49 +0200

    Se cambia la validación para eventos activos y pasados

--- pages/api/getEventos.ts
+++ pages/api/getEventos.ts
@@ -11,12 +11,12 @@ export default withCors(async function handler(req, res) {
     database: process.env.DB_NAME,
   });
 
-  const [rows] = await db.execute('SELECT * FROM eventos WHERE eve_activo = "ACTIVO"');
+  const [rows] = await db.execute('SELECT * FROM eventos WHERE eve_activo = "ACTIVO" ORDER BY eve_fecha ASC');
 
   const ahora = new Date();
 
-  const eventosActivos = (rows as Array<{ eve_fecha: string }>).filter(evento => new Date(evento.eve_fecha) >= ahora);
-  const eventosPasados = (rows as Array<{ eve_fecha: string }>).filter(evento => new Date(evento.eve_fecha) < ahora);
+  const eventosActivos = (rows as Array<{ eve_fecha_fin: string }>).filter(evento => new Date(evento.eve_fecha_fin) >= ahora);
+  const eventosPasados = (rows as Array<{ eve_fecha_fin: string }>).filter(evento => new Date(evento.eve_fecha_fin) < ahora);
 
   res.status(200).json({ eventosActivos, eventosPasados });
 });
